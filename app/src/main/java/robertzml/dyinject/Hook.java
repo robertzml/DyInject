e16fbad01afa4ad73ff0f67d52e304b425a30e73
robertzml <robertzml@126.com>
2018-09-12 22:37:31 +0800

    divide some methods to class.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ app/src/main/java/robertzml/dyinject/Hook.java
@@ -0,0 +1,218 @@
+package robertzml.dyinject;
+
+import android.os.Bundle;
+
+import com.alibaba.fastjson.JSON;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+/*
+ Hook类
+ */
+public class Hook {
+
+    private HttpClient client = new HttpClient();
+
+    private XC_LoadPackage.LoadPackageParam lpparam;
+
+    public Hook(XC_LoadPackage.LoadPackageParam lpparam) {
+        this.lpparam = lpparam;
+    }
+
+    /**
+     * Hook 评论
+     */
+    public void Comment() {
+        findAndHookMethod("com.ss.android.ugc.aweme.comment.api.CommentApi", lpparam.classLoader, "a",
+                String.class, long.class, int.class, int.class, String.class, String.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook Comment Start---");
+                        XposedBridge.log("first = " + param.args[0]);
+                        //XposedBridge.log(param.getResult().toString());
+                        super.beforeHookedMethod(param);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook Comment End---");
+                        XposedBridge.log("first = " + param.args[0]);
+                        Object result = param.getResult();
+
+                        XposedBridge.log(result.toString());
+
+                        String js = JSON.toJSONString(result);
+                        XposedBridge.log(js);
+
+                        String r = client.Post("data", js);
+                        super.afterHookedMethod(param);
+
+                        /*
+                        StringBuilder sbName = new StringBuilder();
+                        StringBuilder sbValue = new StringBuilder();
+                        String[] fieldNames = getFiledName(result);
+
+                        for(int j=0 ; j<fieldNames.length ; j++){     //遍历所有属性
+                            String name = fieldNames[j];    //获取属性的名字
+                            Object value = getFieldValueByName(name, result);
+                            sbName.append(name);
+                            sbValue.append(value);
+                            if(j != fieldNames.length - 1) {
+                                sbName.append("/");
+                                sbValue.append("/");
+                            }
+                        }
+
+                        XposedBridge.log("Field: " + sbName.toString());
+                        XposedBridge.log("Value:" + sbValue.toString());
+                        */
+                    }
+                });
+    }
+
+    // 关注列表
+    private void HookFollowing() {
+        Class<?> c = XposedHelpers.findClass("com.ss.android.ugc.aweme.following.a.c", lpparam.classLoader);
+
+        findAndHookMethod("com.ss.android.ugc.aweme.following.b.c", lpparam.classLoader, "a",
+                c, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook Following Start---");
+                        super.beforeHookedMethod(param);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook Following End---");
+
+                        super.afterHookedMethod(param);
+                    }
+                });
+    }
+
+
+    // 粉丝列表
+    private void HookFollower() {
+
+        Class<?> c = XposedHelpers.findClass("com.ss.android.ugc.aweme.following.a.c", lpparam.classLoader);
+
+        findAndHookMethod("com.ss.android.ugc.aweme.following.b.a", lpparam.classLoader, "a",
+                c, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook Follower Start---");
+                        super.beforeHookedMethod(param);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook Follower End---");
+
+                        Object result = param.getResult();
+
+                        super.afterHookedMethod(param);
+                    }
+                });
+    }
+
+    private void HookFollower2() {
+
+        Class<?> c = XposedHelpers.findClass("com.ss.android.ugc.aweme.following.b.b", lpparam.classLoader);
+
+        findAndHookMethod("com.ss.android.ugc.aweme.app.api.a", lpparam.classLoader, "a",
+                String.class, Class.class, String.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook Follower Start---");
+                        XposedBridge.log("first = " + param.args[0]);
+
+                        // XposedBridge.log(param.args[1].getClass().getName());
+                        super.beforeHookedMethod(param);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook Follower End---");
+
+                        Object result = param.getResult();
+
+                        if (result != null) {
+                            XposedBridge.log(result.toString());
+
+                            String js = JSON.toJSONString(result);
+                            XposedBridge.log(js);
+
+                            String r = client.Post("http://192.168.1.106:5000/data", js);
+                        }
+                        super.afterHookedMethod(param);
+                    }
+                });
+    }
+
+    /**
+     * 粉丝列表
+     */
+    public void HookFollower3() {
+
+    }
+
+    public void HookChatActivity() {
+
+        findAndHookMethod("com.ss.android.ugc.aweme.im.sdk.chat.ChatRoomActivity", lpparam.classLoader, "onCreate",
+                Bundle.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook chat Start---");
+                        super.beforeHookedMethod(param);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("---Hook chat End---");
+
+                        // Object result = param.getResult();
+
+                        super.afterHookedMethod(param);
+                    }
+                });
+    }
+
+
+    /**
+     * 获取属性名数组
+     * */
+    private static String[] getFiledName(Object o){
+        Field[] fields=o.getClass().getDeclaredFields();
+        String[] fieldNames=new String[fields.length];
+        for(int i=0;i<fields.length;i++){
+            // System.out.println(fields[i].getType());
+            fieldNames[i]=fields[i].getName();
+        }
+        return fieldNames;
+    }
+
+    /**
+     * 根据属性名获取属性值
+     * */
+    private static Object getFieldValueByName(String fieldName, Object o) {
+        try {
+            String firstLetter = fieldName.substring(0, 1).toUpperCase();
+            String getter = "get" + firstLetter + fieldName.substring(1);
+            Method method = o.getClass().getMethod(getter, new Class[] {});
+            Object value = method.invoke(o, new Object[] {});
+            return value;
+        } catch (Exception e) {
+
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/robertzml/dyinject/HttpClient.java
@@ -0,0 +1,48 @@
+package robertzml.dyinject;
+
+import java.io.IOException;
+
+import de.robv.android.xposed.XposedBridge;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Http 客户端
+ */
+public class HttpClient {
+
+    //region Field
+    private static final MediaType Media = MediaType.parse("application/json; charset=utf-8");
+
+    private OkHttpClient client = new OkHttpClient();
+
+    private String host = "http://192.168.1.106:5000/";
+    //endregion Field
+
+    /**
+     * 上传抓包内容
+     * @param url 远程地址
+     * @param json 上传数据
+     * @return 返回结果
+     */
+    public String Post(String url, String json) {
+        RequestBody body = RequestBody.create(Media, json);
+        Request request = new Request.Builder()
+                .url(host + url)
+                .post(body)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            XposedBridge.log("Exception: " + e.getMessage());
+        }
+
+        return "";
+    }
+
+
+}
